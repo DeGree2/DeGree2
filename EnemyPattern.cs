19d92adb776e1671470884eaa8ee9464521ac180
DeGree2 <36892178+DeGree2@users.noreply.github.com>
2018-03-12 17:39:55 +0200

    Add files via upload
    Pridėti kelis rutulius, isTrigger= true, tag = bonusHealth, material = bonusHealth.
    Kubui tam kuris turi but žaidėjas tag = player, box collider, is trigger= true.
    Enemy capsule collider.
    Pridėti UI image (man atrodo) HealthBarBackground, spalva juoda.
    Pridėti vaiką (man rodos) HealthBarBarckground'o, HealthBar, nustatymus atsiunčiau.
    Pridėti GameOverPanel, juodos spalvos (šiaip neturi reikšmės), jai text vaiką, kuriame parašyta "GAME OVER".

---
+++ EnemyPattern.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPattern : MonoBehaviour {
+    public Transform[] target;
+    public float speed;
+
+    private int current;
+
+    // Update is called once per frame
+    void Update () {
+        if (transform.position != target[current].position) //move until you reach the current waypoint
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else current = (current + 1) % target.Length; //waypoint reached, move to the next waypoint
+	}
+
+    //Živilė. Player is damaged if touched by enemy
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            HealthBarScript.Damage();
+        }
+    }
+}
